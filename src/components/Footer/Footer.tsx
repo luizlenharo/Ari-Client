c380bad12f5d2b9b9600396a7245c41896b421c4
luiz lenharo <llenharo@ciandt.com>
2025-12-15 11:06:14 -0300

    feat: add directories structure

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+    return (
+        <footer>
+            <p>© 2025 Ari. All rights reserved.</p>
+        </footer>
+    )
+}
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <header>
+      <h1>Welcome to My App</h1>
+    </header>
+  );
+}
